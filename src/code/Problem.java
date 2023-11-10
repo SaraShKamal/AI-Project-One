7b7ee05ec48a13c4da62a82cd2900d867b970481
sara <sara.shkamal2000@gmail.com>
2023-11-10 14:27:29 +0300

    Problem Class

--- src/code/Problem.java
+++ src/code/Problem.java
@@ -6,5 +6,6 @@ import java.util.function.Function;
 public abstract class Problem {
     public State initialState;
     public abstract boolean isGoal(Node node);
-    public abstract ArrayList <Function< Node, Node>> getPossibleOperations(Node node);
+    public abstract ArrayList<Function<Node, Node>> getPossibleOperations(Node node);
+
 }
