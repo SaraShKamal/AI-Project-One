3199467c311724c8d693993bd595a28eef5288f0
Maryam Sherif <mariamsherif2.ms@gmail.com>
2023-11-12 20:33:59 +0200

    testing phase..

--- src/code/LLAPSearch.java
+++ src/code/LLAPSearch.java
@@ -19,77 +19,89 @@ public class LLAPSearch extends GenericSearch {
             // plan, monetaryCost, nodesExpanded
             SearchResult result = bfs.bfsSearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            //loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
         }
         if (Objects.equals(strategy, "DF")){
             DFS dfs = new DFS(actions);
             // plan, monetaryCost, nodesExpanded
             SearchResult result = dfs.dfsSearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
-
         }
         if (Objects.equals(strategy, "ID")){
             IDS ids = new IDS(actions) ;
             // plan, monetaryCost, nodesExpanded
             SearchResult result = ids.idsSearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            //loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
-
         }
         //ucs
         if (Objects.equals(strategy, "UC")){
@@ -97,26 +109,30 @@ public class LLAPSearch extends GenericSearch {
             // plan, monetaryCost, nodesExpanded
             SearchResult result = ucs.ucsSearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            //loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
-
         }
         //greedy1
         if (Objects.equals(strategy, "GR1")){
@@ -124,25 +140,30 @@ public class LLAPSearch extends GenericSearch {
             // plan, monetaryCost, nodesExpanded
             SearchResult result = greedy1.greedySearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+//loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
         }
         //greedy2
         if (Objects.equals(strategy, "GR2")){
@@ -150,25 +171,30 @@ public class LLAPSearch extends GenericSearch {
             // plan, monetaryCost, nodesExpanded
             SearchResult result = greedy2.greedySearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            //loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
         }
         //astar1
         if (Objects.equals(strategy, "AS1")){
@@ -176,25 +202,30 @@ public class LLAPSearch extends GenericSearch {
             // plan, monetaryCost, nodesExpanded
             SearchResult result = aStar1.aStarSearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            //loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
         }
         //astar2
         if (Objects.equals(strategy, "AS2")){
@@ -202,43 +233,45 @@ public class LLAPSearch extends GenericSearch {
             // plan, monetaryCost, nodesExpanded
             SearchResult result = aStar2.aStarSearch(initialState);
             //loop through the plan and put it in a string separated by ,
-            for (int i = 0; i < result.getPlan().size(); i++) {
-                if (i == result.getPlan().size()-1){
-                    resultString += result.getPlan().get(i);
-                }
-                else {
-                    resultString += result.getPlan().get(i) + ",";
-                }
+            //loop through the plan and put it in a string separated by ,
+            if (result == null){
+                return "NOSOLUTION";
             }
-            //monetary cost
-            resultString += ";" + result.getMonetaryCost();
-            //nodes expanded
-            resultString += ";" + result.getNodesExpanded();
-            if (visualize){
-                //loop on statesString reverse and add to string
-                for (int i = result.getStatesString().size()-1; i >= 0; i--) {
-                    resultString += "\n" + result.getStatesString().get(i);
+            else {
+                for (int i = 0; i < result.getPlan().size(); i++) {
+                    if (i == result.getPlan().size() - 1) {
+                        resultString += result.getPlan().get(i);
+                    } else {
+                        resultString += result.getPlan().get(i) + ",";
+                    }
                 }
+                //monetary cost
+                resultString += ";" + result.getMonetaryCost();
+                //nodes expanded
+                resultString += ";" + result.getNodesExpanded();
+                if (visualize) {
+                    //loop on statesString reverse and add to string
+                    for (int i = result.getStatesString().size() - 1; i >= 0; i--) {
+                        resultString += "\n" + result.getStatesString().get(i);
+                    }
+                }
+                return resultString;
             }
-            return resultString;
-        }
-        if (resultString.equals("") || resultString.equals(null)){
-            return "NOSOLUTION";
         }
         return "NOSOLUTION";
     }
 
-    public static void main(String[] args) {
-        String init ="50;"+
-                "22,22,22;" +
-                "50,60,70;" +
-                "30,2;19,1;15,1;" +
-                "300,5,7,3,20;" +
-                "500,8,6,3,40;";  // Replace with your initial state string
-        String strategy = "GR1"; // Choose your strategy
-        boolean visualize = true; // Set to true to visualize
-
-        String result = solve(init, strategy, visualize);
-        System.out.println(result);
-    }
+//    public static void main(String[] args) {
+//        String init ="50;"+
+//                "22,22,22;" +
+//                "50,60,70;" +
+//                "30,2;19,1;15,1;" +
+//                "300,5,7,3,20;" +
+//                "500,8,6,3,40;";  // Replace with your initial state string
+//        String strategy = "AS1"; // Choose your strategy
+//        boolean visualize = true; // Set to true to visualize
+//
+//        String result = solve(init, strategy, visualize);
+//        System.out.println(result);
+//    }
 }
