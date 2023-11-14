b6322d441960c8882f1adc2948047eebb49ca285
Maryam Sherif <mariamsherif2.ms@gmail.com>
2023-11-14 17:02:47 +0200

    GR1 done

--- src/code/Greedy1Comparator.java
+++ src/code/Greedy1Comparator.java
@@ -1,25 +1,19 @@
 package code;
 import java.util.Comparator;
 public class Greedy1Comparator  implements Comparator<State> {
-        @Override
-        public int compare(State o1, State o2) {
-            int distance1 = estimateDistanceToGoal(o1);
-            int distance2 = estimateDistanceToGoal(o2);
+    @Override
+    public int compare(State o1, State o2) {
+        int distance1 = estimateDistanceToGoal(o1);
+        int distance2 = estimateDistanceToGoal(o2);
 
-            // Compare in ascending order based on the estimated distance to the goal
-            return Integer.compare(distance1, distance2);
-        }
+        // Compare in ascending order based on the estimated distance to the goal
+        return Integer.compare(distance1, distance2);
+    }
 
-        private int estimateDistanceToGoal(State state) {
-            // Implement your heuristic to estimate the remaining distance to the goal
-            // Combine both prosperity and total money spent in a way that suits your problem
-            int prosperityDistance = Math.max(0, 100 - state.getProsperity());
-            int moneySpentDistance = state.getMoney_spent(); // Adjust this based on your State class
+    private int estimateDistanceToGoal(State state) {
+        // Implement your heuristic to estimate the remaining distance to the goal
+        return Math.max(0, 100 - state.getProsperity());
 
-            // You might use a weighted sum or other combination logic
-            // For now, let's use a simple sum for demonstration purposes
-            return prosperityDistance + moneySpentDistance;
-
-        }
+    }
     }
 
--- src/code/UCSComparator.java
+++ src/code/UCSComparator.java
@@ -6,12 +6,16 @@ public class UCSComparator implements Comparator<State> {
 
     @Override
     public int compare(State o1, State o2) {
-        if (o1.getMoney_spent() < o2.getMoney_spent()) {
-            return -1; //state1 has lower cost, so it comes first
-        } else if (o1.getMoney_spent() > o2.getMoney_spent()) {
-            return 1; //state2 has lower cost, so it comes first
+        double ratio1 = (double) o1.getProsperity() / o1.getMoney_spent();
+        double ratio2 = (double) o2.getProsperity() / o2.getMoney_spent();
+
+        // Compare based on the ratio of prosperity increase to money spent
+        if (ratio1 > ratio2) {
+            return -1; // State 1 has a higher ratio, so it comes first
+        } else if (ratio1 < ratio2) {
+            return 1; // State 2 has a higher ratio, so it comes first
         } else {
-            return 0; //both states have the same cost
+            return 0;
         }
     }
 }
